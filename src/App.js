2e2dfdc8df27fd2312a7fbfb4ab9985b90b17057
DANNIEL <donghoon149@gmail.com>
2020-09-30 18:27:23 +0900

    add: add foodList feature

--- src/App.js
+++ src/App.js
@@ -18,14 +18,17 @@ const chartStyle = {
     margin: '0 auto',
 }
 function App() {
-    const imageRef = useRef()
+    // const imageRef = useRef()
     // const [shouldClassify, setShouldClassify] = useState(false);
+    const foodList = JSON.parse(localStorage.getItem('foodList')) || []
     const [imageFile, setImageFile] = useState()
-    const [backgroundImage, setBackgroundImage] = useState()
-    const [category, setCatetory] = useState()
+    const [backgroundImage, setBackgroundImage] = useState([])
+    const [category, setCategory] = useState(foodList)
     const [menu, setMenu] = useState([])
     const [foodTime, setFoodTime] = useState('전체')
     const [isLoading, setIsLoading] = useState(false)
+    const [isReady, setIsReady] = useState(false)
+
     useEffect(() => {
         if (!isLoading) {
             setTimeout(() => {
@@ -35,30 +38,50 @@ function App() {
         classifier = ml5.imageClassifier('./my-model/model.json', () => {
             console.log('model loaded!')
         })
-        if (backgroundImage && classifier) {
-            console.log(imageRef.current)
+        if (!isEmpty(backgroundImage) && !isEmpty(classifier) && isReady) {
             console.log(backgroundImage)
-            let imageElement = document.createElement('img')
-            imageElement.src = backgroundImage
-            console.log(imageElement)
-            classifier.classify(imageElement, (error, results) => {
-                if (error) {
-                    console.error(error)
-                    return
+            const promises = []
+            const classifyFunction = async () => {
+                for (let i = 0; i < backgroundImage.length; i++) {
+                    promises.push(
+                        new Promise(() => {
+                            let imageElement = document.createElement('img')
+                            imageElement.src = backgroundImage[i]
+                            console.log('dd')
+                            classifier.classify(
+                                imageElement,
+                                (error, results) => {
+                                    if (error) {
+                                        console.error(error)
+                                        return
+                                    }
+                                    results.sort((a, b) =>
+                                        a.confidence < b.confidence ? 1 : -1
+                                    )
+                                    console.log(results)
+                                    setCategory((prev) => [
+                                        ...prev,
+                                        results[0].label,
+                                    ])
+                                }
+                            )
+                        })
+                    )
                 }
-                console.log('test start!')
-                console.log(results)
-                results.sort((a, b) => (a.confidence < b.confidence ? 1 : -1))
-                console.log(results)
-                // setBackgroundImage()
-                setCatetory(results[0].label)
-                // setMenu(menu => menu.concat(JSON.parse(menu.data)));
-                // setMenu({ ...menu, label: results[0].label })
-            })
+                await Promise.all(promises)
+            }
+
+            classifyFunction()
+            setIsReady(false)
+            setBackgroundImage([])
         }
         console.log(menu)
-    }, [backgroundImage])
+    }, [backgroundImage, isReady])
+    useEffect(() => {
+        localStorage.setItem('foodList', JSON.stringify(category))
 
+        console.log(category)
+    }, [category])
     return (
         <React.Fragment>
             {!isLoading ? (
@@ -93,7 +116,7 @@ function App() {
                             justifyContent: 'center',
                         }}
                     ></div> */}
-                    <div className="food-time-wrap">
+                    {/* <div className="food-time-wrap">
                         <div
                             className={`food-time-btn${
                                 foodTime === '전체' ? '-clicked' : ''
@@ -138,7 +161,7 @@ function App() {
                         >
                             <span>저녁</span>
                         </div>
-                    </div>
+                    </div> */}
                     <div
                         style={{
                             width: '100%',
@@ -159,7 +182,6 @@ function App() {
                         height={230}
                         colorRange={myColor}
                     />
-
                     <FoodList />
                     <input
                         accept="image/*"
@@ -169,40 +191,108 @@ function App() {
                         }}
                         id="contained-button-file"
                         type="file"
+                        multiple
                         onChange={(e) => {
-                            var file = e.target.files[0]
-                            setImageFile(file)
-                            const reader = new FileReader()
-                            reader.readAsDataURL(file)
-                            reader.onloadend = () => {
-                                setBackgroundImage([reader.result])
-                                console.log(reader.result)
+                            const imageList = []
+                            const promises = []
+                            const solve = async () => {
+                                for (
+                                    let i = 0;
+                                    i < e.target.files.length;
+                                    i++
+                                ) {
+                                    promises.push(
+                                        new Promise(() => {
+                                            var reader = new FileReader()
+                                            reader.readAsDataURL(
+                                                e.target.files[i]
+                                            )
+
+                                            reader.onloadend = () => {
+                                                setBackgroundImage((prev) => [
+                                                    ...prev,
+                                                    reader.result,
+                                                ])
+                                                // imageList.push(reader.result)
+                                            }
+                                        })
+                                    )
+                                }
+                                await Promise.all(promises).then(() => {})
                             }
+                            solve()
+
+                            // for (let i = 0; i < e.target.files.length; i++) {
+                            //     var reader = new FileReader()
+                            //     testList.push('t')
+                            //     reader.readAsDataURL(e.target.files[i])
+                            //     console.log(e.target.files[i])
+                            //     reader.onloadend = () => {
+                            //         imageList.push(reader.result)
+                            //         setBackgroundImage(imageList)
+                            //         console.log(reader.result)
+                            //     }
+                            //     console.log(imageList)
+                            //     console.log(testList)
+                            // }
                         }}
                     />
-                    {isEmpty(backgroundImage) && (
-                        <label
-                            // className={classes.photoArea}
-                            htmlFor="contained-button-file"
-                            className="plus-wrap"
-                        >
-                            <img className="plus" src="/plus.png" />
-                        </label>
-                    )}
+                    <label
+                        // className={classes.photoArea}
+                        htmlFor="contained-button-file"
+                        className="plus-wrap"
+                    >
+                        <img className="plus" src="/plus.png" />
+                    </label>
+                    <div
+                        style={{
+                            padding: '20px',
+                            display: 'flex',
+                            margin: '0 auto',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        {!isEmpty(backgroundImage) &&
+                            backgroundImage.map((data, i) => {
+                                return (
+                                    <>
+                                        <img
+                                            src={data}
+                                            style={{
+                                                width: '100px',
+                                            }}
+                                            alt="이미지"
+                                            key={i}
+                                        />
+                                    </>
+                                )
+                            })}
+                    </div>{' '}
                     {!isEmpty(backgroundImage) && (
-                        <>
-                            <img
-                                src={backgroundImage}
-                                style={{
-                                    width: '300px',
-                                    margin: '0 auto',
-                                }}
-                                alt="이미지"
-                                ref={imageRef}
-                            />
-                        </>
+                        <div
+                            onClick={() => {
+                                setIsReady(true)
+                            }}
+                            style={{
+                                margin: '0 auto',
+                                display: 'flex',
+                                width: '280px',
+                                height: '58px',
+                                textAlign: 'center',
+                                fontSize: '18px',
+                                backgroundColor: '#254F54',
+                                borderRadius: '20px',
+                                color: 'white',
+                                fontWeight: ' bold',
+                                boxShadow: '0 4px 14px 0 rgba(0,0,0,0.3)',
+                                cursor: 'pointer',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                            }}
+                        >
+                            추가하기
+                        </div>
                     )}
-
                     {/* {!isEmpty(menu) && (
                 <div>
                     {menu.map((data, i) => {
@@ -214,14 +304,81 @@ function App() {
                     })}
                 </div>
             )} */}
+                    <div
+                        style={{
+                            margin: '0 auto',
+                            maxWidth: '375px',
+                            textAlign: 'center',
+                        }}
+                    >
+                        {!isEmpty(category) &&
+                            category.map((data, i) => {
+                                console.log(i)
+                                return (
+                                    <div
+                                        style={{
+                                            display: 'flex',
+                                            justifyContent: 'space-between',
+                                            fontSize: '18px',
+                                            paddingLeft: '20px',
+                                            paddingRight: '20px',
+                                            paddingBottom: ' 20px',
+                                            paddingTop: '20px',
+                                            borderBottom: '1px solid #f5f5f5',
+                                        }}
+                                    >
+                                        <span>{data}</span>
+                                        <div>
+                                            <span>2인분</span>
+                                            <span
+                                                style={{
+                                                    paddingLeft: '10px',
+                                                    color: 'red',
+                                                }}
+                                                onClick={() => {
+                                                    console.log(category)
+                                                    let idx = category.indexOf(
+                                                        data
+                                                    )
+
+                                                    console.log(idx)
+                                                    if (idx > -1) {
+                                                        category.splice(idx, 1)
+                                                        setCategory([
+                                                            ...category,
+                                                        ])
+                                                    }
+                                                }}
+                                            >
+                                                삭제
+                                            </span>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                    </div>
                     {!isEmpty(category) && (
                         <div
+                            onClick={() => {}}
                             style={{
-                                fontSize: '40px',
+                                margin: '0 auto',
+                                display: 'flex',
+                                width: '280px',
+                                height: '58px',
+                                textAlign: 'center',
+                                fontSize: '18px',
+                                backgroundColor: '#254F54',
+                                borderRadius: '20px',
+                                color: 'white',
+                                fontWeight: ' bold',
+                                boxShadow: '0 4px 14px 0 rgba(0,0,0,0.3)',
+                                marginTop: '20px',
+                                cursor: 'pointer',
+                                alignItems: 'center',
+                                justifyContent: 'center',
                             }}
                         >
-                            <div>{category}</div>
-                            <>500kcal</>
+                            분석하기
                         </div>
                     )}
                 </>
